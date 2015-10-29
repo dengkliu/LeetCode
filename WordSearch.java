90807e2dba6442f3b07e3bf99b966aab8c5ae9b9
Dengke Liu <dengkeliu92@gmail.com>
2015-10-28 19:44:36 -0500

    Word Search Problem
    Iterate through the list, call the function identify if the char matches
    that in the string. In the function,  if the char matches, then change
    it state to be visited, and go on call the function on the four
    sourrounding chars, if one of them match, go on, if all of them don't
    match, return false and go back to last function stack and change the
    char state to unvisited, which means it is not a part of the word and we
    can check it later.

--- WordSearch.java
+++ WordSearch.java
@@ -1,45 +1,26 @@
 public class WordSearch {
 
     // Declare a global array of boolean
-    static boolean[][] visited;
-
-    // first find the cell that contains the same char as word.charAt(0)
-    public boolean exist(char[][] board, String word) {
-
-        // initilize the boolean array
-        visited = new boolean[board.length][board[0].length];
-
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[i].length; j++){
-                if((word.charAt(0) == board[i][j]) && search(board, word, i, j, 0)){
-                    return true;
-                    // if we find the cell, and if we can find the whole word in the adjacent cells, we win
-                }
-            }
+  public boolean exist(char[][] board, String word) {
+    char[] w = word.toCharArray();
+    for (int y=0; y<board.length; y++) {
+        for (int x=0; x<board[y].length; x++) {
+            if (exist(board, y, x, w, 0)) return true;
         }
-
-        return false;
     }
+    return false;
+}
 
-    private boolean search(char[][]board, String word, int i, int j, int index){
-
-        if(index == word.length()){
-            return true;
-        }
-
-        if(i >= board.length || i < 0 || j >= board[i].length || j < 0 || board[i][j] != word.charAt(index) || visited[i][j]){
-            return false;
-        }
-
-        visited[i][j] = true;
-        if(search(board, word, i-1, j, index+1) || 
-           search(board, word, i+1, j, index+1) ||
-           search(board, word, i, j-1, index+1) || 
-           search(board, word, i, j+1, index+1)){
-            return true;
-        }
-
-        visited[i][j] = false;
-        return false;
-    }
+private boolean exist(char[][] board, int y, int x, char[] word, int i) {
+    if (i == word.length) return true;
+    if (y<0 || x<0 || y == board.length || x == board[y].length) return false;
+    if (board[y][x] != word[i]) return false;
+    board[y][x] ^= 256;
+    boolean exist = exist(board, y, x+1, word, i+1)
+        || exist(board, y, x-1, word, i+1)
+        || exist(board, y+1, x, word, i+1)
+        || exist(board, y-1, x, word, i+1);
+    board[y][x] ^= 256;
+    return exist;
+}
 }
