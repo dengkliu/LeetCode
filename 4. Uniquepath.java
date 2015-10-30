9b2b65879fea6154fa5f37e467a01f5d6bb101c4
Dengke Liu <dengkeliu92@gmail.com>
2015-10-30 01:03:23 -0500

    Unique Path using DP

--- 4. Uniquepath.java
+++ 4. Uniquepath.java
@@ -1,23 +1,19 @@
 public class Uniquepath {
+    
     public int uniquePaths(int m, int n) {
+        int[] arr = new int[m];
         
-        if(n==1||m==1) return 1;
-        if(n==0||m==0) return 0;
-        
-        long result=1;
-        
-        if(m<n){int t=m; m=n; n=t;}
-        
-        for(int i=0; i<n-1; i++){
-            result*=n+m-2;
-            m--;
+        for (int i = 0; i < m; i++) {
+            arr[i] = 1;
         }
         
-        for(int i=1; i<=n-1; i++){
-            result/=i;
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                arr[j] = arr[j] + arr[j-1];
+            }
         }
         
-        return (int) result;
+        return arr[m-1];
     }
 }
     
