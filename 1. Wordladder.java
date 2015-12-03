b39becb4e55f7c5f584df2089856eeea2c0b602d
Dengke Liu <dengkeliu92@gmail.com>
2015-12-03 00:53:17 -0600

    Build word ladder
    Start: hit
    End: cog
    Set: [hot, dot, lot, dog, log]
    Return:
    [
    [hit, hot, lot, log, cog]
    [hit, hot,dot,dog, cog]
    ]

--- 1. Wordladder.java
+++ 1. Wordladder.java
@@ -1,9 +1,13 @@
 public class WordLadder {
+
     Map<String,List<String>> map;
     List<List<String>> results;
+
     public List<List<String>> findLadders(String start, String end, Set<String> dict) {     
+        
         results= new ArrayList<List<String>>();
-        if (dict.size() == 0)
+        
+        if (dict.size() == 0) // no words in the dict to use
             return results;
 
         int min=Integer.MAX_VALUE;
@@ -14,7 +18,9 @@ public class WordLadder {
         map = new HashMap<String,List<String>>();
 
         Map<String,Integer> ladder = new HashMap<String,Integer>();
-        dict.add(end);
+
+        dict.add(end); // make sure the end word is in the set
+
         for (String string:dict)
             ladder.put(string, Integer.MAX_VALUE);
         ladder.put(start, 0);
@@ -25,31 +31,37 @@ public class WordLadder {
 
             String word = queue.poll();
 
-            int step = ladder.get(word)+1;//'step' indicates how many steps are needed to travel to one word. 
+            int step = ladder.get(word)+1; //'step' indicates how many steps are needed to travel to one word. 
 
             if (step>min) break;
 
             for (int i = 0; i < word.length(); i++){
-               StringBuilder builder = new StringBuilder(word); 
+
+               StringBuilder builder = new StringBuilder(word); // user Stringbuilder to modify the word without changing the original word
+                
                 for (char ch='a';  ch <= 'z'; ch++){
+
                     builder.setCharAt(i,ch);
-                    String new_word=builder.toString();             
+                    String new_word=builder.toString(); 
+
                     if (ladder.containsKey(new_word)) {
 
-                        if (step>ladder.get(new_word))//Check if it is the shortest path to one word.
+                        if (step>ladder.get(new_word)) // make sure that we don't consider the word itself
                             continue;
+
                         else if (step<ladder.get(new_word)){
                             queue.add(new_word);
                             ladder.put(new_word, step);
-                        }else;// It is a KEY line. If one word already appeared in one ladder,
-                              // Do not insert the same word inside the queue twice. Otherwise it gets TLE.
+                        }else;
+                        // It is a KEY line. If one word already appeared in one ladder,
+                        // Do not insert the same word inside the queue twice. Otherwise it gets TLE.
 
                         if (map.containsKey(new_word)) //Build adjacent Graph
                             map.get(new_word).add(word);
                         else{
                             List<String> list= new LinkedList<String>();
                             list.add(word);
-                            map.put(new_word,list);
+                            map.put(new_word, list);
                             //It is possible to write three lines in one:
                             //map.put(new_word,new LinkedList<String>(Arrays.asList(new String[]{word})));
                             //Which one is better?
@@ -57,7 +69,6 @@ public class WordLadder {
 
                         if (new_word.equals(end))
                             min=step;
-
                     }//End if dict contains new_word
                 }//End:Iteration from 'a' to 'z'
             }//End:Iteration from the first to the last
@@ -69,7 +80,7 @@ public class WordLadder {
 
         return results;        
     }
-    private void backTrace(String word,String start,List<String> list){
+    private void backTrace(String word, String start, List<String> list){ //backtracking
         if (word.equals(start)){
             list.add(0,start);
             results.add(new ArrayList<String>(list));
